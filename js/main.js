b1d6e3e5c5e149a8f78ccdcc93d408631a0e2f97
HEESUNae <heesun9456@gmail.com>
2023-03-25 19:49:56 +0900

    23/03/25 컨텐츠 퍼블리싱 완료

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+import ipads from "../data/ipads.js";
+
+// 장바구니
 const basketStarterEl = document.querySelector("header .basket-starter");
 const basketEl = basketStarterEl.querySelector(".basket");
 
@@ -24,3 +27,108 @@ const showBasket = () => {
 const hideBasket = () => {
   basketEl.classList.remove("show");
 };
+
+// 검색
+const headerEl = document.querySelector("header");
+const headerMenuEls = [...headerEl.querySelectorAll("ul.menu > li")];
+const searchWrapEl = headerEl.querySelector(".search-wrap");
+const searchStarterEl = headerEl.querySelector(".search-starter");
+const searchCloserEl = searchWrapEl.querySelector(".search-closer");
+const searchShadowEl = searchWrapEl.querySelector(".shadow");
+const searchInputEl = searchWrapEl.querySelector("input");
+const searchDelayEls = [...searchWrapEl.querySelectorAll("li")];
+
+const showSearch = () => {
+  headerEl.classList.add("searching");
+  document.documentElement.classList.add("fixed");
+
+  headerMenuEls.reverse().forEach((el, index) => {
+    el.style.transitionDelay = (index * 0.4) / headerMenuEls.length + "s";
+  });
+
+  searchDelayEls.forEach((el, index) => {
+    el.style.transitionDelay = (index * 0.4) / searchDelayEls.length + "s";
+  });
+
+  setTimeout(() => {
+    searchInputEl.focus();
+  }, 600);
+};
+
+const hideSearch = () => {
+  headerEl.classList.remove("searching");
+  document.documentElement.classList.remove("fixed");
+
+  headerMenuEls.reverse().forEach((el, index) => {
+    el.style.transitionDelay = (index * 0.4) / headerMenuEls.length + "s";
+  });
+
+  searchDelayEls.reverse().forEach((el, index) => {
+    el.style.transitionDelay = (index * 0.4) / searchDelayEls.length + "s";
+  });
+  searchDelayEls.reverse();
+  searchInputEl.value = "";
+};
+
+searchStarterEl.addEventListener("click", showSearch);
+searchCloserEl.addEventListener("click", hideSearch);
+searchShadowEl.addEventListener("click", hideSearch);
+
+// 요소의 가시성 관찰
+const io = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    }
+    entry.target.classList.add("show");
+  });
+});
+
+const infoEls = document.querySelectorAll(".info");
+infoEls.forEach((el) => {
+  io.observe(el);
+});
+
+// 비디오 재생!
+const video = document.querySelector(".stage video");
+const playBtn = document.querySelector(".stage .controller--play");
+const pauseBtn = document.querySelector(".stage .controller--pause");
+
+playBtn.addEventListener("click", () => {
+  video.play();
+  playBtn.classList.add("hide");
+  pauseBtn.classList.remove("hide");
+});
+pauseBtn.addEventListener("click", () => {
+  video.pause();
+  playBtn.classList.remove("hide");
+  pauseBtn.classList.add("hide");
+});
+
+// "당신에게 맞는 iPad는? 렌더링"
+const itemsEl = document.querySelector("section.compare .items");
+
+ipads.forEach((ipad) => {
+  const itemEl = document.createElement("div");
+  itemEl.classList.add("item");
+
+  let colorList = "";
+  ipad.colors.forEach((color) => {
+    colorList += `<li style="background-color:${color}"></li>`;
+  });
+
+  itemEl.innerHTML = /* html */ `
+    <div class="thumbnail">
+      <img src="${ipad.thumbnail}" alt="${ipad.name}"/>
+    </div>
+    <ul class="colors">
+      ${colorList}
+    </ul>
+    <h3 class="name">${ipad.name}</h3>
+    <p class="tagline">${ipad.tagline}</p>
+    <p class="price">₩${ipad.price.toLocaleString("en-us")}</p>
+    <button class="btn">구입하기</button>
+    <a href="${ipad.url}" class="link">더 알아보기</a>
+  `;
+  itemsEl.append(itemEl);
+});
